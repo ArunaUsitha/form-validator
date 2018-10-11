5bece374b05a8c97fa14cf416138d7ae122b72aa
Killer <18212520+Psy-Ko@users.noreply.github.com>
2018-10-11 17:27:54 +0530

    Added support for radio button validation

--- form-validator/js/formValidator.js
+++ form-validator/js/formValidator.js
@@ -33,16 +33,19 @@
  *
  *
  */
-var validator = function validateForm(parameters) {
+let validator = function validateForm(parameters) {
 
 
     let formID = parameters.formID;
     let animate = parameters.animate;
+    let debugMode = parameters.debugMode;
     let cssClass = 'animate shake';
+
+    (debugMode == true) ? debugLog('FormValidator Debug Mode Enabled. Debugging....!', '') : '';
 
     function shake(formID) {
         if (animate !== undefined) {
-            $(formID).addClass(cssClass).delay(1000).queue(function(next){
+            $(formID).addClass(cssClass).delay(1000).queue(function (next) {
                 $(this).removeClass(cssClass);
                 next();
             });
@@ -50,55 +53,91 @@ var validator = function validateForm(parameters) {
     }
 
 
-    var results = {};
-    var status = true;
-
-    function addStyles(selector, response) {
-        selector.parent().find('.verror').html(response).css({
-            'font-size': '11px',
-            // 'color': '#D81159'
-        });
-        selector.parent().parent().addClass('has-error');
+    function debugLog(text, data) {
+        if (debugMode == true) {
+            console.log(text);
+            console.log(data)
+        }
     }
 
-    function removeStyles(selector) {
-        selector.parent().find('.verror').html('').css({
-            'font-size': '11px',
-            // 'color': '#D81159'
-        });
-        selector.parent().parent().removeClass('has-error');
+    let results = {};
+    let status = true;
+
+    function addStyles(selector, response, type) {
+
+        if (type == 'radio') {
+            selector.parent().find('.verror').html(response).css({
+                'font-size': '11px',
+            })
+            selector.parent().addClass('has-error');
+        } else {
+            selector.parent().find('.verror').html(response).css({
+                'font-size': '11px',
+                // 'color': '#D81159'
+            });
+            selector.parent().parent().addClass('has-error');
+        }
+
+
+    }
+
+    function removeStyles(selector, type) {
+        if (type == 'radio') {
+            selector.parent().find('.verror').html('').css({
+                'font-size': '11px',
+            })
+            selector.parent().removeClass('has-error');
+        } else {
+            selector.parent().find('.verror').html('').css({
+                'font-size': '11px',
+                // 'color': '#D81159'
+            });
+            selector.parent().parent().removeClass('has-error');
+        }
     }
 
 
     // check and validate
     function checkAndValidate(dataAttributes, element) {
 
-
+        debugLog('Main Validator Method :  ', element)
 
         if (element[0].id !== '') {
 
-            var attributes = dataAttributes.split(',');
+            let attributes = dataAttributes.split(',');
 
             $.each(attributes, function (index, value) {
 
-
                 //required
                 if (value === 'required') {
-                    if (element.val() === '') {
-                        addStyles(element, 'This field is required!');
-                        results[element[0].id] = false;
+
+                    // added support for text area propper validation
+                    if (element[0].tagName && element[0].tagName.toLowerCase() == "textarea") {
+
+                        if (/^\s*$/g.test(element.val())) {
+                            addStyles(element, 'This field is required!');
+                            results[element[0].id] = false;
+                        } else {
+                            removeStyles(element);
+                            results[element[0].id] = true;
+                        }
+
                     } else {
-                        removeStyles(element);
-                        results[element[0].id] = true;
+                        if (element.val() == '') {
+                            addStyles(element, 'This field is required!');
+                            results[element[0].id] = false;
+                        } else {
+                            removeStyles(element);
+                            results[element[0].id] = true;
+                        }
                     }
-                    // return false;
 
                 }
 
 
                 //length
                 if ((/length/).test(value)) {
-                    var length = value.split('=')[1];
+                    let length = value.split('=')[1];
                     if (!(element.val().length > length)) {
                         addStyles(element, 'This value should be ' + length + ' characters');
                         results[element[0].id] = false;
@@ -110,8 +149,8 @@ var validator = function validateForm(parameters) {
 
                 //telephone no
                 if (value === 'telephone') {
-                    console.log(element)
-                    var regx = /^[0-9]+$/;
+
+                    let regx = /^[0-9]+$/;
                     if (element.val().length > 0) {
                         if (element.val().length < 10 || element.val().length > 10 || !(element.val().match(regx))) {
                             addStyles(element, 'Enter a valid phone No (10 characters excluding +94)');
@@ -121,8 +160,7 @@ var validator = function validateForm(parameters) {
                             results[element[0].id] = true;
                         }
                     } else {
-                        // removeStyles(element);
-                        // results[element[0].id] = true;
+                     
                     }
                 }
 
@@ -130,7 +168,7 @@ var validator = function validateForm(parameters) {
                 if (value === 'email') {
 
                     if (element.val().length > 0) {
-                        var emil = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        let emil = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                         if (!(element.val().match(emil))) {
                             addStyles(element, 'Enter valid Email Address');
                             results[element[0].id] = false;
@@ -149,7 +187,7 @@ var validator = function validateForm(parameters) {
                 if (value === 'password') {
 
                     if (element.val() !== '') {
-                        var conf = $(element).parent().parent().parent().find('[data-validate*="passwordconfirm"]');
+                        let conf = $(element).parent().parent().parent().find('[data-validate*="passwordconfirm"]');
                         if (!(element.val() === conf.val())) {
                             addStyles(element, 'Passwords doesn\'t match');
                             addStyles(conf, 'Passwords doesn\'t match');
@@ -168,7 +206,7 @@ var validator = function validateForm(parameters) {
 
 
                     if (element.val() !== '') {
-                        var num = /^\d*$/;
+                        let num = /^\d*$/;
 
                         if (!(element.val().match(num))) {
                             addStyles(element, 'Please enter only numbers');
@@ -177,6 +215,28 @@ var validator = function validateForm(parameters) {
                             removeStyles(element);
                             results[element[0].id] = true;
                         }
+                    }
+
+                }
+
+
+                // for radio button
+                if (value === 'radioBt') {
+                    let btList = $(element).closest('.validate-radio-button-area').find('input:radio[name=' + element[0].name + ']');
+                    let r = false;
+
+                    $.each(btList, function (key, value) {
+                        if ($(value).prop('checked')) {
+                            r = true;
+                        }
+                    });
+
+                    if (r) {
+                        results[element[0].id] = true;
+                        removeStyles(element, 'radio');
+                    } else {
+                        results[element[0].id] = false;
+                        addStyles(element, 'Please select option', 'radio');
                     }
 
                 }
@@ -190,16 +250,21 @@ var validator = function validateForm(parameters) {
 
     }
 
-    var clss = '.validate';
+    let clss = '.validate';
 
-    var inputs = $(formID + ' ' + clss);
+    let inputs = $(formID + ' ' + clss);
+
+    debugLog('form ID', formID);
+    debugLog('validate class', clss);
+
+    debugLog('Inputs List', inputs);
 
     if (inputs.length > 0) {
 
         // noinspection JSAnnotator
         $.each(inputs, function (index, value) {
 
-            var selector = $(this);
+            let selector = $(this);
             try {
                 checkAndValidate($(this).data('validate'), selector);
             } catch (e) {
@@ -210,16 +275,16 @@ var validator = function validateForm(parameters) {
         })
     }
 
-    console.log(results)
 
     $.each(results, function (index, value) {
-
 
         if (value == false) {
             status = false;
             shake(formID);
             return false
         }
+
+
     });
 
 
