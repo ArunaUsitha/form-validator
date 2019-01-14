5a13e826f653f6b34cc8fb7fccc3a728a9cc3bd5
Killer <18212520+Psy-Ko@users.noreply.github.com>
2019-01-14 10:02:52 +0530

    On the fly validation support

--- form-validator/js/formValidator.js
+++ form-validator/js/formValidator.js
@@ -2,115 +2,73 @@
  * @Author :Aruna|Killer
  * used to validate html input elements on the fly
  * of a form
- *
- * required Jquery library 2.00 or higher
- *
- * usage :
- *  inputs should be in a form group, the bootstrap way. as example
- *   <form id="form-to-validate">
- *  <div class="custom-form-user">
- *  <div class="form-group">
- *  <label for="testInput" class="col-sm-2 control-label">test input</label>
- *  <div class="col-sm-8">
- *  <input type="text" class="form-control validate"  name="testInput" id="testInput" placeholder="testInput" data-validate="required">
- *  <span class="verror"></span>
- *  </div>
- *  </div>
- *  </div>
- *  </form>
- *
- *  for the input field that you want to validate add the class 'validate'
- *   then add the types you want to validate Ex: password,required,length as comma seperated data attributes
- *
- *   data-validate = 'required,length=10,password'
- *
- *  then call the method using the form id
- *
- *  validateForm('#form-to-validate');
- *
- *  and the script shoud be included on the bottom of the page
- *
- *
- *
- */
-let validator = function validateForm(parameters) {
+*/
 
 
-    let formID = parameters.formID;
-    let animate = parameters.animate;
-    let debugMode = parameters.debugMode;
-    let cssClass = 'animate shake';
+let results = {};
+let status = true;
 
-    (debugMode == true) ? debugLog('FormValidator Debug Mode Enabled. Debugging....!', '') : '';
+let debugMode
 
-    function shake(formID) {
-        if (animate !== undefined) {
-            $(formID).addClass(cssClass).delay(1000).queue(function (next) {
-                $(this).removeClass(cssClass);
-                next();
-            });
-        }
+function addStyles(selector, response, type) {
+
+    if (type == 'radio') {
+
+        selector.siblings('.verror').html(response).css({
+            'font-size': '11px',
+        })
+        selector.closest('.form-group').addClass('has-error');
+    } else {
+
+        selector.closest('.form-group').find('.verror').html(response).css({
+            'font-size': '11px',
+            // 'color': '#D81159'
+        });
+        selector.closest('.form-group').addClass('has-error');
     }
 
 
-    function debugLog(text, data) {
-        if (debugMode == true) {
-            console.log(text);
-            console.log(data)
-        }
+}
+
+function removeStyles(selector, type) {
+    if (type == 'radio') {
+        selector.closest('.form-group').find('.verror').html('').css({
+            'font-size': '11px',
+        })
+        selector.closest('.form-group').removeClass('has-error');
+    } else {
+        selector.closest('.form-group').find('.verror').html('').css({
+            'font-size': '11px',
+            // 'color': '#D81159'
+        });
+        selector.closest('.form-group').removeClass('has-error');
     }
-
-    let results = {};
-    let status = true;
-
-    function addStyles(selector, response, type) {
-
-        if (type == 'radio') {
-            selector.siblings('.verror').html(response).css({
-                'font-size': '11px',
-            })
-            selector.closest('.form-group').addClass('has-error');
-        } else {
-            selector.siblings('.verror').html(response).css({
-                'font-size': '11px',
-                // 'color': '#D81159'
-            });
-            selector.closest('.form-group').addClass('has-error');
-        }
+}
 
 
+function debugLog(text, data) {
+    if (debugMode == true) {
+        console.log(text);
+        console.log(data)
     }
-
-    function removeStyles(selector, type) {
-        if (type == 'radio') {
-            selector.siblings('.verror').html('').css({
-                'font-size': '11px',
-            })
-            selector.closest('.form-group').removeClass('has-error');
-        } else {
-            selector.siblings('.verror').html('').css({
-                'font-size': '11px',
-                // 'color': '#D81159'
-            });
-            selector.closest('.form-group').removeClass('has-error');
-        }
-    }
+}
 
 
-    // check and validate
-    function checkAndValidate(dataAttributes, element) {
+// check and validate
+function checkAndValidate(dataAttributes, element) {
 
-        debugLog('Main Validator Method :  ', element)
+    // debugLog('Main Validator Method :  ', element)
 
-        if (element[0].id !== '') {
+    if (element[0].id !== '') {
 
-            if (element.is(":visible")) {
+        if (element.is(":visible")) {
 
-                let attributes = dataAttributes.split(',');
-                $.each(attributes, function (index, value) {
+            let attributes = dataAttributes.split(',');
+            $.each(attributes, function (index, value) {
 
                     //required
                     if (value === 'required') {
+
 
                         // added support for text area propper validation
                         if (element[0].tagName && element[0].tagName.toLowerCase() == "textarea") {
@@ -123,11 +81,16 @@ let validator = function validateForm(parameters) {
                                 results[element[0].id] = true;
                             }
 
+
                         } else {
-                            if (element.val() == '' || element.val() == null) {
+
+
+                            if (element.val() == '' || element.val() == null || element.val().trim() == '') {
                                 addStyles(element, 'This field is required!');
                                 results[element[0].id] = false;
+
                             } else {
+
                                 removeStyles(element);
                                 results[element[0].id] = true;
                             }
@@ -188,7 +151,8 @@ let validator = function validateForm(parameters) {
                     if (value === 'password') {
 
                         if (element.val() !== '') {
-                            let conf = $(element).parent().parent().parent().find('[data-validate*="passwordconfirm"]');
+                            let conf = $(element).parent().parent().find('[data-validate*="passwordconfirm"]');
+
                             if (!(element.val() === conf.val())) {
                                 addStyles(element, 'Passwords doesn\'t match');
                                 addStyles(conf, 'Passwords doesn\'t match');
@@ -201,6 +165,23 @@ let validator = function validateForm(parameters) {
                         }
 
                     }
+
+                    if (value === 'passwordconfirm') {
+
+                        if (element.val() !== '') {
+                            let conf = $(element).parent().parent().find('[data-validate*="password"]');
+                            if (!(element.val() === conf.val())) {
+                                addStyles(element, 'Passwords doesn\'t match');
+                                addStyles(conf, 'Passwords doesn\'t match');
+                                results[element[0].id] = false;
+                            } else {
+                                removeStyles(element);
+                                removeStyles(conf);
+                                results[element[0].id] = true;
+                            }
+                        }
+                    }
+
 
                     //number
                     if (value === 'number') {
@@ -242,18 +223,90 @@ let validator = function validateForm(parameters) {
 
                     }
 
+                    //date
+                    if (value === 'date') {
 
-                });
 
-            } else {
-                removeStyles(element);
-            }
+                        function isValidDate(dateString) {
+                            // First check for the pattern
+                            var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
+
+                            if (!regex_date.test(dateString)) {
+                                return false;
+                            }
+
+                            // Parse the date parts to integers
+                            var parts = dateString.split("-");
+                            var day = parseInt(parts[2], 10);
+                            var month = parseInt(parts[1], 10);
+                            var year = parseInt(parts[0], 10);
+
+                            // Check the ranges of month and year
+                            if (year < 1000 || year > 3000 || month == 0 || month > 12) {
+                                return false;
+                            }
+
+                            var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+                            // Adjust for leap years
+                            if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+                                monthLength[1] = 29;
+                            }
+
+                            // Check the range of the day
+                            return day > 0 && day <= monthLength[month - 1];
+                        }
+
+
+                        if (!isValidDate(element.val())) {
+                            addStyles(element, 'Please enter correct date');
+                            results[element[0].id] = false;
+                        } else {
+                            removeStyles(element);
+                            results[element[0].id] = true;
+                        }
+
+
+                    }
+
+
+                }
+            );
+
+        } else {
+            removeStyles(element);
         }
-        else {
-            throw "Critical error, Elements ID not defined in : " + element.attr('name') + ". validation falied.!";
-            status = false;
+    } else {
+        throw "Critical error, Elements ID not defined in : " + element.attr('name') + ". validation falied.!";
+        status = false;
+    }
+}
+
+
+let validator = function validateForm(parameters) {
+
+
+    results = {};
+    status = true;
+
+
+    let formID = parameters.formID;
+    let animate = parameters.animate;
+    debugMode = parameters.debugMode;
+    let cssClass = 'animate shake';
+
+
+    (debugMode == true) ? debugLog('FormValidator Debug Mode Enabled. Debugging....!', '') : '';
+
+    function shake(formID) {
+        if (animate !== false) {
+            $(formID).addClass(cssClass).delay(1000).queue(function (next) {
+                $(this).removeClass(cssClass);
+                next();
+            });
         }
     }
+
 
     let clss = '.validate';
 
@@ -292,10 +345,101 @@ let validator = function validateForm(parameters) {
 
     });
 
-
+    debugLog('status', status)
+    debugLog('results', results)
     return status;
 
 
 };
+
+
+function getEventListners(obj) {
+
+    var c = $._data(obj, "events").keyup;
+    // console.log(c)
+    return c
+
+}
+
+
+let eventListners = function () {
+
+    let eventListner = [];
+    let object;
+
+    let get = function (obj) {
+        object = obj;
+
+        let el = $._data(obj, "events").keyup;
+
+        if (el) {
+            if (el.length) {
+                if (el.length > 0) {
+
+                    $.each(el, function (k, v) {
+
+                        eventListner.push(v.handler)
+
+                    })
+
+                }
+
+            }
+        }
+
+
+    }
+
+
+    let reAttach = function () {
+
+        $.each(eventListner, function (k, v) {
+            $(document).on('keyup', object, function () {
+                v
+            })
+        })
+
+    }
+
+
+    return {
+        get: get,
+        reAttach: reAttach
+    }
+}()
+
+
+//realtime validations
+$(document).on('change keyup click', '.validate-realTime', function (e) {
+    results = {};
+    status = true;
+    let isDate = false;
+
+    let attributes = $(this).data('validate').split(',')
+    $.each(attributes, function (k, v) {
+        if (v == 'date') {
+            isDate = true
+        }
+    })
+
+    if (isDate) {
+        eventListners.get(this)
+
+        $(this).off('keyup');
+        $(this).off('keydown');
+        $(this).off('change');
+
+    }
+
+    //main check
+    checkAndValidate($(this).data('validate'), $(this));
+
+    if (isDate) {
+        eventListners.reAttach()
+    }
+
+
+})
+
 
 
